b09919b7080e79aa0d8938025deb6e2d3cbdc3fa
dalai <dalaitseren.g@gmail.com>
2020-09-11 23:08:04 +0800

    winner todruulah

--- app_t.js
+++ app_t.js
@@ -30,15 +30,42 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
     document.getElementById("current-" + activePlayer).textContent = roundScore;
   } else {
     // Хэрэв идэвхтэй тоглогч нь 0 байвал идэвхтэй тоглогчийг 1 болго.
-    roundScore = 0;
-    document.getElementById("current-" + activePlayer).textContent = 0;
-    // Үгүй бол идэвхтэй тоглогчийг 0 болго.
-
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-
-    // улаан цэгийг хайж олно.
-    document.querySelector(".player-0-panel").classList.toggle("active");
-    document.querySelector(".player-1-panel").classList.toggle("active");
-    // diceDom.style.display = "none";
+    switchToNextPlayer();
   }
 });
+
+// hold товчны эвэнт
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  scores[activePlayer] = scores[activePlayer] + roundScore;
+
+  document.getElementById("score-" + activePlayer).textContent =
+    scores[activePlayer];
+  // Уг тоглогчийг хожсон эсэхийг шалгах Оноо 100-аас их
+  if (scores[activePlayer] >= 20) {
+    // ялагч
+    document.getElementById("name-" + activePlayer).textContent = "ЯЛАГЧ!!!";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
+  } else {
+    switchToNextPlayer();
+  }
+});
+
+// дараагийн тоглогчруу шилжүүлнэ.
+function switchToNextPlayer() {
+  // ээлжийн оноог 0 болгоно
+  roundScore = 0;
+  document.getElementById("current-" + activePlayer).textContent = roundScore;
+
+  // Үгүй бол идэвхтэй тоглогчийг 0 болго.
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+  // улаан цэгийг хайж олж солино.
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+  // diceDom.style.display = "none";
+}
