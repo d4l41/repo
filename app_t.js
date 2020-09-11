5e91c2d2e9faf240904c4ab3b931219ed853c39b
dalai <dalaitseren.g@gmail.com>
2020-09-11 22:13:09 +0800

    eelj solidog bolgoh

--- app_t.js
+++ app_t.js
@@ -1,5 +1,5 @@
 var scores = [0, 0];
-var activePlayer = 1;
+var activePlayer = 0;
 var roundScore = 0;
 var diceNumber = Math.floor(Math.random() * 6 + 1);
 
@@ -11,9 +11,34 @@ document.getElementById("current-1").textContent = 0;
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
+// Шоог шидэх эвэнт листенер
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 хүртэл тоог санамсаргүйгээр гаргах
   var diceNumber = Math.floor(Math.random() * 6 + 1);
 
+  // шооны зургийг вэб дээр гаргаж ирнэ.
   diceDom.style.display = "block";
+
+  // Буусан санамсаргүй тоонд харгалзах зургийг вэб дээр гаргаж ирнэ.
   diceDom.src = "dice-" + diceNumber + ".png";
+
+  // буусан тоо нь 1-ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ.
+
+  if (diceNumber !== 1) {
+    // 1-ээс ялгаатай тоо буулаа.
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    // Хэрэв идэвхтэй тоглогч нь 0 байвал идэвхтэй тоглогчийг 1 болго.
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    // Үгүй бол идэвхтэй тоглогчийг 0 болго.
+
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    // улаан цэгийг хайж олно.
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    // diceDom.style.display = "none";
+  }
 });
